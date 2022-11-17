1b86e47347dd743aaa801947bbc7e01a90431619
Bombim <mxujoy@gmail.com>
2022-11-17 12:57:01 -0500

    Initial Commit

---
+++ XcodeCloudTest/XcodeCloudTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeCloudTestApp.swift
+//  XcodeCloudTest
+//
+//  Created by Michael Xu on 11/17/22.
+//
+
+import SwiftUI
+
+@main
+struct XcodeCloudTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
